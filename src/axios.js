71fb17a124528d632ea259fab0dfea9c8691c0dd
ssaurabh8778 <ssaurabh8778@gmail.com>
2020-11-25 22:43:23 +0530

    new commit

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  //backend api rooturl
-  //baseURL: "http://localhost:5000/",
-  //baseURL: "https://us-central1-pdfdetails191120.cloudfunctions.net/app",
   baseURL: "http://127.0.0.1:8000/api",
-  //baseURL: "https://saurabh8778.pythonanywhere.com/api",
 });
 
 export default instance;
